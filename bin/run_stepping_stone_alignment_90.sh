1f07384ceb9ddabc56477dad4726196921716707
Jamie Oaks <joaks1@gmail.com>
2017-08-02 12:33:56 -0500

    Adding shell scripts for stepping-stone analyses.
    Adding shell scripts for performing stepping-stone analyses on each
    simulated observed alignments.

---
+++ bin/run_stepping_stone_alignment_90.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=3:00:00
+#PBS -j oe
+#PBS -l jobflags=ADVRES:jro0014_lab.56281
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    module load gcc/5.3.0
+fi
+
+aln_num=90
+out_path="run_stepping_stone_alignment_${aln_num}.out"
+
+for run_num in 1 2 3
+do
+    rng_seed=$RANDOM
+    ./run_stepping_stone.sh -a $aln_num -r $run_num -s $rng_seed 1>"$out_path" 2>&1
+done
